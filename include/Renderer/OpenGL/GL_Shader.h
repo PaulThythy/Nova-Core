d4590d107b596019418b763ea0146e190e04f5ff
PaulThythy <paul.thi@hotmail.fr>
2025-11-10 18:27:01 +0100

    replaced glew with glad;added GL_Shader functions for shader compilation

---
+++ include/Renderer/OpenGL/GL_Shader.h
@@ -0,0 +1,19 @@
+#ifndef GL_SHADER_HPP
+#define GL_SHADER_HPP
+
+#include <glad/gl.h>
+#include <string>
+
+namespace Nova::Renderer::OpenGL {
+
+    std::string ReadFile(const std::string& filePath);
+
+    GLuint LoadRenderShader(const std::string& vertexPath, const std::string& fragmentPath);
+    GLuint LoadComputeShader(const std::string& computePath);
+
+    GLuint CompileShader(GLenum shaderType, const std::string& shaderCode);
+    GLuint LinkProgram(const std::initializer_list<GLuint>& shaderIDs);
+
+} // namespace Nova::Renderer::OpenGL
+
+#endif // GL_SHADER_HPP
---
+++ src/Renderer/OpenGL/GL_Shader.cpp
@@ -0,0 +1,110 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "Renderer/OpenGL/GL_Shader.h"
+
+namespace Nova::Renderer::OpenGL {
+
+    std::string ReadFile(const std::string& filePath) {
+        std::ifstream file(filePath);
+        if (!file.is_open())
+        {
+            std::cerr << "Failed to open shader file: " << filePath << std::endl;
+            return "";
+        }
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        file.close();
+        return buffer.str();
+    }
+
+    GLuint CompileShader(GLenum shaderType, const std::string& shaderCode) {
+        if (shaderCode.empty())
+            return 0;
+
+        const char* sourcePtr = shaderCode.c_str();
+        GLuint shaderID = glCreateShader(shaderType);
+        glShaderSource(shaderID, 1, &sourcePtr, nullptr);
+        glCompileShader(shaderID);
+
+        GLint compileStatus;
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compileStatus);
+        if (compileStatus == GL_FALSE) {
+            GLint logLength;
+            glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &logLength);
+
+            std::string log(logLength, '\0');
+            glGetShaderInfoLog(shaderID, logLength, nullptr, &log[0]);
+            std::cerr << "Shader compilation error:\n" << log << std::endl;
+
+            glDeleteShader(shaderID);
+            return 0;
+        }
+        return shaderID;
+    }
+
+    GLuint LinkProgram(const std::initializer_list<GLuint>& shaderIDs) {
+        GLuint programID = glCreateProgram();
+
+        for (auto sid : shaderIDs)
+        {
+            if (sid != 0)
+                glAttachShader(programID, sid);
+        }
+
+        glLinkProgram(programID);
+
+        GLint linkStatus;
+        glGetProgramiv(programID, GL_LINK_STATUS, &linkStatus);
+        if (linkStatus == GL_FALSE)
+        {
+            GLint logLength;
+            glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &logLength);
+
+            std::string log(logLength, '\0');
+            glGetProgramInfoLog(programID, logLength, nullptr, &log[0]);
+            std::cerr << "Program linking error:\n" << log << std::endl;
+
+            glDeleteProgram(programID);
+            return 0;
+        }
+
+        for (auto sid : shaderIDs)
+        {
+            if (sid != 0)
+            {
+                glDetachShader(programID, sid);
+                glDeleteShader(sid);
+            }
+        }
+
+        return programID;
+    }
+
+    GLuint LoadRenderShader(const std::string& vertexPath, const std::string& fragmentPath) {
+        std::string vertexCode = ReadFile(vertexPath);
+        GLuint vertexID = CompileShader(GL_VERTEX_SHADER, vertexCode);
+        if (!vertexID)
+            return 0;
+
+        std::string fragmentCode = ReadFile(fragmentPath);
+        GLuint fragmentID = CompileShader(GL_FRAGMENT_SHADER, fragmentCode);
+        if (!fragmentID)
+            return 0;
+
+        GLuint programID = LinkProgram({ vertexID, fragmentID });
+        return programID;
+    }
+
+    GLuint LoadComputeShader(const std::string& computePath) {
+        std::string computeCode = ReadFile(computePath);
+        GLuint computeID = CompileShader(GL_COMPUTE_SHADER, computeCode);
+        if (!computeID)
+            return 0;
+
+        GLuint programID = LinkProgram({ computeID });
+        return programID;
+    }
+} // namespace Nova::Renderer::OpenGL
