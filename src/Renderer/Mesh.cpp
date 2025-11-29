1d8b1eb41e29ccfff75150cecb093c3cde20e68e
PaulThythy <paul.thi@hotmail.fr>
2025-11-29 17:03:02 +0100

    y axis goes upward !

--- src/Renderer/Mesh.cpp
+++ src/Renderer/Mesh.cpp
@@ -18,26 +18,26 @@ namespace Nova::Core::Renderer {
         vertices.reserve(4);
 
         Vertex v0;
-        v0.m_Position = { -1.0f, -1.0f, 0.0f };
-        v0.m_Normal = { 0.0f,  0.0f, 1.0f };
+        v0.m_Position = { -1.0f, 0.0f, -1.0f };
+        v0.m_Normal = { 0.0f,  1.0f, 0.0f };
         v0.m_TexCoord = { 0.0f,  0.0f };
         v0.m_Color = { 1.0f,  0.0f, 0.0f }; // red
 
         Vertex v1;
-        v1.m_Position = { 1.0f, -1.0f, 0.0f };
-        v1.m_Normal = { 0.0f,  0.0f, 1.0f };
+        v1.m_Position = { 1.0f, 0.0f, -1.0f };
+        v1.m_Normal = { 0.0f,  1.0f, 0.0f };
         v1.m_TexCoord = { 1.0f,  0.0f };
         v1.m_Color = { 0.0f,  1.0f, 0.0f }; // green
 
         Vertex v2;
-        v2.m_Position = { 1.0f,  1.0f, 0.0f };
-        v2.m_Normal = { 0.0f,  0.0f, 1.0f };
+        v2.m_Position = { 1.0f,  0.0f, 1.0f };
+        v2.m_Normal = { 0.0f,  1.0f, 0.0f };
         v2.m_TexCoord = { 1.0f,  1.0f };
         v2.m_Color = { 0.0f,  0.0f, 1.0f }; // blue
 
         Vertex v3;
-        v3.m_Position = { -1.0f,  1.0f, 0.0f };
-        v3.m_Normal = { 0.0f,  0.0f, 1.0f };
+        v3.m_Position = { -1.0f,  0.0f, 1.0f };
+        v3.m_Normal = { 0.0f,  1.0f, 0.0f };
         v3.m_TexCoord = { 0.0f,  1.0f };
         v3.m_Color = { 1.0f,  1.0f, 1.0f }; // white
 
@@ -47,8 +47,8 @@ namespace Nova::Core::Renderer {
         vertices.push_back(v3);
 
         std::vector<int> indices = {
-            0, 1, 2,
-            2, 3, 0
+            0, 2, 1,
+            3, 2, 0
         };
 
         return std::make_shared<Mesh>(std::move(vertices), std::move(indices));
