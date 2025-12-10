f73f80a8228410dbbcb48a1195e12a631024c179
PaulThythy <paul.thi@hotmail.fr>
2025-12-10 05:16:17 +0100

    Cube modelisation added

--- src/Renderer/Mesh.cpp
+++ src/Renderer/Mesh.cpp
@@ -12,8 +12,7 @@ namespace Nova::Core::Renderer {
 
     void Mesh::Unbind() const {}
 
-    std::shared_ptr<Mesh> Mesh::CreatePlane()
-    {
+    std::shared_ptr<Mesh> Mesh::CreatePlane(){
         std::vector<Vertex> vertices;
         vertices.reserve(4);
 
@@ -50,6 +49,129 @@ namespace Nova::Core::Renderer {
             0, 2, 1,
             3, 2, 0
         };
+
+        return std::make_shared<Mesh>(std::move(vertices), std::move(indices));
+    }
+
+    std::shared_ptr<Mesh> Mesh::CreateCube(float halfExtent) {
+        std::vector<Vertex> vertices;
+        std::vector<int>    indices;
+
+        vertices.reserve(24);  // 6 faces * 4 vertices per face
+        indices.reserve(36);   // 6 faces * 2 triangles * 3 indices
+
+        const float h = halfExtent;
+        const glm::vec3 colors[3] = {
+            {1.0f, 0.0f, 0.0f}, // red
+            {0.0f, 1.0f, 0.0f}, // green
+            {0.0f, 0.0f, 1.0f}  // blue
+        };
+
+        auto addFace = [&](const glm::vec3& p0,
+                           const glm::vec3& p1,
+                           const glm::vec3& p2,
+                           const glm::vec3& p3,
+                           const glm::vec3& normal)
+        {
+            // p0..p3 are assumed in CCW order when seen from outside
+            // We will add triangles with CW order later via indices
+
+            int baseIndex = static_cast<int>(vertices.size());
+
+            Vertex v0, v1, v2, v3;
+            v0.m_Position = p0;
+            v1.m_Position = p1;
+            v2.m_Position = p2;
+            v3.m_Position = p3;
+
+            v0.m_Normal = v1.m_Normal = v2.m_Normal = v3.m_Normal = normal;
+
+            // Simple UVs for each face
+            v0.m_TexCoord = {0.0f, 0.0f};
+            v1.m_TexCoord = {1.0f, 0.0f};
+            v2.m_TexCoord = {1.0f, 1.0f};
+            v3.m_TexCoord = {0.0f, 1.0f};
+
+            // Assign RGB colors in a repeating pattern over vertices
+            Vertex* v[4] = { &v0, &v1, &v2, &v3 };
+            for (int i = 0; i < 4; ++i) {
+                int idx = baseIndex + i;
+                v[i]->m_Color = colors[idx % 3];
+            }
+
+            vertices.push_back(v0);
+            vertices.push_back(v1);
+            vertices.push_back(v2);
+            vertices.push_back(v3);
+
+            int i0 = baseIndex + 0;
+            int i1 = baseIndex + 1;
+            int i2 = baseIndex + 2;
+            int i3 = baseIndex + 3;
+
+            // Triangles in CW order
+            indices.push_back(i0);
+            indices.push_back(i1);
+            indices.push_back(i2);
+
+            indices.push_back(i0);
+            indices.push_back(i2);
+            indices.push_back(i3);
+        };
+
+        // Front face  (Z+)
+        addFace(
+            { -h, -h,  h }, // bottom-left
+            {  h, -h,  h }, // bottom-right
+            {  h,  h,  h }, // top-right
+            { -h,  h,  h }, // top-left
+            {  0.0f, 0.0f, 1.0f }
+        );
+
+        // Back face   (Z-)
+        addFace(
+            { -h, -h, -h }, // bottom-left
+            { -h,  h, -h }, // top-left
+            {  h,  h, -h }, // top-right
+            {  h, -h, -h }, // bottom-right
+            {  0.0f, 0.0f,-1.0f }
+        );
+
+        // Right face  (X+)
+        addFace(
+            {  h, -h, -h }, // bottom-left
+            {  h,  h, -h }, // top-left
+            {  h,  h,  h }, // top-right
+            {  h, -h,  h }, // bottom-right
+            {  1.0f, 0.0f, 0.0f }
+        );
+
+        // Left face   (X-)
+        addFace(
+            { -h, -h,  h }, // bottom-left
+            { -h,  h,  h }, // top-left
+            { -h,  h, -h }, // top-right
+            { -h, -h, -h }, // bottom-right
+            { -1.0f, 0.0f, 0.0f }
+        );
+
+        // Top face    (Y+)
+        addFace(
+            { -h,  h, -h }, // bottom-left
+            { -h,  h,  h }, // bottom-right
+            {  h,  h,  h }, // top-right
+            {  h,  h, -h }, // top-left
+            {  0.0f, 1.0f, 0.0f }
+        );
+
+        // Bottom face (Y-)
+        addFace(
+            { -h, -h,  h }, // bottom-left
+            { -h, -h, -h }, // bottom-right
+            {  h, -h, -h }, // top-right
+            {  h, -h,  h }, // top-left
+            {  0.0f,-1.0f, 0.0f }
+        );
 
         return std::make_shared<Mesh>(std::move(vertices), std::move(indices));
     }
