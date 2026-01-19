a362f567e0f019526ba99b6a5b3c3a94602d1cef
PaulThythy <paul.thi@hotmail.fr>
2026-01-19 23:02:35 +0100

    spacing issue resolved

--- include/Renderer/Backends/Vulkan/VK_Renderer.h
+++ include/Renderer/Backends/Vulkan/VK_Renderer.h
@@ -33,42 +33,42 @@ namespace Nova::Core::Renderer::Backends::Vulkan {
 		void EndFrame() override;
 
 	private:
-                // --- Helpers ---
-                bool CreateSwapchain();
-                bool CreateImageViews();
-                bool CreateRenderPass();
-                bool CreateFramebuffers();
-                bool CreateCommandPool();
-                bool AllocateCommandBuffers();
-                bool CreateSyncObjects();
-                bool CreateImGuiDescriptorPool();
-                void CleanupSwapchain();
-                bool RecreateSwapchain();
+        // --- Helpers ---
+        bool CreateSwapchain();
+        bool CreateImageViews();
+        bool CreateRenderPass();
+        bool CreateFramebuffers();
+        bool CreateCommandPool();
+        bool AllocateCommandBuffers();
+        bool CreateSyncObjects();
+        bool CreateImGuiDescriptorPool();
+        void CleanupSwapchain();
+        bool RecreateSwapchain();
 
-                VK_Instance       m_VKInstance;
-                VK_Device         m_VKDevice;
+        VK_Instance       m_VKInstance;
+        VK_Device         m_VKDevice;
 
-                VkSwapchainKHR                m_Swapchain = VK_NULL_HANDLE;
-                VkFormat                      m_SwapchainImageFormat{};
-                VkExtent2D                    m_SwapchainExtent{};
-                uint32_t                      m_MinImageCount = 0;
-                std::vector<VkImage>          m_SwapchainImages;
-                std::vector<VkImageView>      m_SwapchainImageViews;
-                std::vector<VkFramebuffer>    m_SwapchainFramebuffers;
+        VkSwapchainKHR                m_Swapchain = VK_NULL_HANDLE;
+        VkFormat                      m_SwapchainImageFormat{};
+        VkExtent2D                    m_SwapchainExtent{};
+        uint32_t                      m_MinImageCount = 0;
+        std::vector<VkImage>          m_SwapchainImages;
+        std::vector<VkImageView>      m_SwapchainImageViews;
+        std::vector<VkFramebuffer>    m_SwapchainFramebuffers;
 
-                VkRenderPass       m_RenderPass = VK_NULL_HANDLE;
+        VkRenderPass       m_RenderPass = VK_NULL_HANDLE;
 
-                VkCommandPool                   m_CommandPool = VK_NULL_HANDLE;
-                std::vector<VkCommandBuffer>    m_CommandBuffers;
+        VkCommandPool                   m_CommandPool = VK_NULL_HANDLE;
+        std::vector<VkCommandBuffer>    m_CommandBuffers;
 
-                VkSemaphore       m_ImageAvailableSemaphore = VK_NULL_HANDLE;
-                VkSemaphore       m_RenderFinishedSemaphore = VK_NULL_HANDLE;
-                VkFence           m_InFlightFence = VK_NULL_HANDLE;
+        VkSemaphore       m_ImageAvailableSemaphore = VK_NULL_HANDLE;
+        VkSemaphore       m_RenderFinishedSemaphore = VK_NULL_HANDLE;
+        VkFence           m_InFlightFence = VK_NULL_HANDLE;
 
-                VkDescriptorPool  m_ImGuiDescriptorPool = VK_NULL_HANDLE;
+        VkDescriptorPool  m_ImGuiDescriptorPool = VK_NULL_HANDLE;
 
-                uint32_t          m_CurrentImageIndex = 0;
-                bool              m_FramebufferResized = false;
+        uint32_t          m_CurrentImageIndex = 0;
+        bool              m_FramebufferResized = false;
 	};
 
 } // namespace Nova::Core::Renderer::Backends::Vulkan
